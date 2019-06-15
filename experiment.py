da851be6e69beea93caeae1aee351b5544b75ba9
Anmol Sharma <anmsharm@Anmols-MacBook-Pro.local>
2019-06-15 21:01:29 +0530

    removed unnecassary comments in experiment.py

--- experiment.py
+++ experiment.py
@@ -5,8 +5,8 @@ import indexing
 import search
 
 if __name__=='__main__':
-    # spliData.split_data_set()
-    # classification.train_model()
+    spliData.split_data_set()
+    classification.train_model()
     confusionMatrix.get_confusion_matrix()
-    # indexing.index_files()
-    # search.search_sample()
+    indexing.index_files()
+    search.search_sample()
